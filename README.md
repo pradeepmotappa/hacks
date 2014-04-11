74078ea4eff96394c6f4c7137cb282426e3c89a2
Rohith Reddy <rohith.reddi9@gmail.com>
2014-04-12 04:54:12 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Bunch of COnfigs and Scripts 
---
+++ scriptz/synclient.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+sleep 15
+/usr/bin/synclient 'TapButton2'=2 'TapButton3'=3
+/usr/bin/notify-send "Two Finger tap for  Middle Click and three for right"
